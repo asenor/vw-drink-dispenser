d7a170a4f8fb2c59ae3eb30b643d3362a4db2f99
Adrián Señor <adrian.senor@gmail.com>
2023-10-16 16:58:51 +0200

    test: dispense two products in a row

--- src/testIntegration/java/com/vw/drink/dispenser/DispenseTest.java
+++ src/testIntegration/java/com/vw/drink/dispenser/DispenseTest.java
@@ -3,21 +3,22 @@ package com.vw.drink.dispenser;
 import com.vw.drink.dispenser.application.dispenser.Dispense;
 import com.vw.drink.dispenser.application.dispenser.InsertCoin;
 import com.vw.drink.dispenser.application.dispenser.SelectProduct;
-import com.vw.drink.dispenser.domain.money.Coin;
-import com.vw.drink.dispenser.domain.time.Timestamp;
 import com.vw.drink.dispenser.domain.dispenser.Dispenser;
 import com.vw.drink.dispenser.domain.dispenser.exception.DispenserNotAvailableException;
-import com.vw.drink.dispenser.domain.product.exception.InvalidProductException;
-import com.vw.drink.dispenser.domain.product.exception.NoUnexpiredProductException;
 import com.vw.drink.dispenser.domain.dispenser.exception.ProductNotSelectedException;
+import com.vw.drink.dispenser.domain.money.Coin;
 import com.vw.drink.dispenser.domain.product.Product;
 import com.vw.drink.dispenser.domain.product.ProductType;
+import com.vw.drink.dispenser.domain.product.exception.InvalidProductException;
+import com.vw.drink.dispenser.domain.product.exception.NoUnexpiredProductException;
+import com.vw.drink.dispenser.domain.time.Timestamp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.vw.drink.dispenser.configuration.TimeConfiguration.NOW_SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DispenseTest extends BaseIntegrationTest {
 
@@ -31,12 +32,22 @@ public class DispenseTest extends BaseIntegrationTest {
     private Dispense dispense;
 
     @Test
-    public void dispense() throws InvalidProductException, DispenserNotAvailableException, ProductNotSelectedException, NoUnexpiredProductException {
-        productRepository.add(Product.Factory.create(ProductType.WATER, new Timestamp(NOW_SECONDS + 1)));
+    public void dispenseTwoProductsAndExhaustStock() throws InvalidProductException, DispenserNotAvailableException, ProductNotSelectedException, NoUnexpiredProductException {
+        productRepository.add(Product.Factory.create(ProductType.WATER, new Timestamp(NOW_SECONDS + 10)));
+        productRepository.add(Product.Factory.create(ProductType.WATER, new Timestamp(NOW_SECONDS + 20)));
 
         selectProduct.handle(ProductType.WATER);
 
         insertCoin.handle(Coin.EURO_2);
+
+        dispense.handle();
+
+        assertTrue(productRepository.hasStock(ProductType.WATER));
+        assertEquals(Dispenser.Status.AVAILABLE, dispenser.status());
+
+        selectProduct.handle(ProductType.WATER);
+
+        insertCoin.handle(Coin.EURO_1);
 
         dispense.handle();
 
